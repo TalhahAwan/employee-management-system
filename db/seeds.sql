9d9bc0a715fb606778ff9cebab0a6b7142f28671
Talhah Awan <talhahawan@gmail.com>
2022-01-17 23:24:32 -0600

    added departments into seeds and templates

---
+++ db/seeds.sql
@@ -0,0 +1,22 @@
+INSERT INTO departments (name)
+VALUES 
+    ("Artist"),
+    ("producer"),
+    ("Agent"),
+    ("Label Owner");
+
+
+INSERT INTO roles (title, salary, departments_id)
+VALUES 
+    ("JCole", "5000000", 1),
+    ("MetroBoomin", "1000000", 2),
+    ("JCole", "5000000", 1),
+    ("JCole", "5000000", 1),
+
+
+INSERT INTO departments (name)
+VALUES 
+    ("Artist"),
+    ("producer"),
+    ("Agent"),
+    ("Label Owner");
