d61c344747f81e5b79928cdc58ca784aad0edd88
Talhah Awan <talhahawan@gmail.com>
2022-01-18 20:57:43 -0600

    created connection

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
 const cTable = require("console.table");
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: 'admin',
+      database: 'work_db'
+    },
+    console.log(`Connected to the work_db database.`)
+  );
+  
